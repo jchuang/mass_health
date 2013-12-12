6d96958978ca8cf33235938693cbc38dbfd44896
Janet Chuang <janet.chuang@gmail.com>
2013-12-11 22:14:37 -0500

    seed data from csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'csv'
+
+TownHealthRecord.delete_all
+
+CSV.foreach(Rails.root + 'db/data/mass_health_data.csv', headers: true) do |row|
+
+  unless row[0].nil? || row[1].nil?
+
+    TownHealthRecord.create(
+      town_name: row[0],
+      population: row[1].gsub(/\W/,'').to_i,
+      youth_population: row[2].gsub(/\W/,'').to_i,
+      senior_population: row[3].gsub(/\W/,'').to_i,
+      per_capita_income: row[4].gsub(/\W/,'').to_i,
+      poverty_population: row[5].gsub(/\W/,'').to_i,
+      poverty_percent: row[6].to_f,
+      prenatal_percent: row[7].to_f,
+      c_section_percent: row[8].to_f,
+      infant_deaths: row[9].to_i,
+      infant_mortality: row[10].to_f,
+      low_birthweight_percent: row[11].to_f,
+      multiple_births_percent: row[12].to_f,
+      public_prenatal_percent: row[13].to_f,
+      teen_birth_percent: row[14].to_f
+      )
+  end
+
+end
